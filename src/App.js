39246f3173d7654eff51eebf727b7bc73e90612a
Ben Poh <ben.poh@section.io>
2022-12-08 14:52:13 +1100

    Setup for hosting React app on Section
    Signed-off-by: Ben Poh <ben.poh@section.io>

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Deploy a React App on Section
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.section.io/docs/tutorials/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Docs
         </a>
       </header>
     </div>
