ea085ed1a4109d9dcd5fa44f71c0fc425b4afd5e
coreysn <95398533+coreysn@users.noreply.github.com>
2023-04-19 12:35:12 -0600

    Add welcome page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import ReactLogo from './react-logo.svg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Deploy a React App on Section
-        </p>
-        <a
-          className="App-link"
-          href="https://www.section.io/docs/tutorials/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Docs
-        </a>
-      </header>
-    </div>
+    <>
+        <div className="w-full max-w-xs">
+            <img src={ReactLogo} alt="React Logo" />
+        </div>
+
+        <div className="w-full max-w-fit bg-white/10 text-sm font-mono leading-none overflow-x-auto rounded-md pl-2 py-3 mt-8 lg:mt-10">
+            <p className="whitespace-nowrap">
+                Get started by editing <span className="font-semibold mr-2">public/index.html</span>
+            </p>
+        </div>
+    </>
   );
 }
 
